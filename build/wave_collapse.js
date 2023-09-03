a4243f5371cfb4fe95a60c26633fe40ad8445391
Jakub Vítovec <jakvitov@gmail.com>
2023-09-03 13:11:55 +0200

    Tile representations edited

--- build/wave_collapse.js
+++ build/wave_collapse.js
@@ -1,28 +1,27 @@
-var imgSize = 48;
-var SCALE = 1;
-var SIDE_SIZE = 720;
-var Tile = /** @class */ (function () {
-    function Tile(type) {
+const imgSize = 48;
+const SCALE = 1;
+const SIDE_SIZE = 720;
+class Tile {
+    constructor(type) {
         this.type = type;
     }
     //Draw the tile, the x,y are left-upper corner
-    Tile.prototype.draw = function (x, y, context) {
-        var imgId = "frame" + this.type;
-        var imgElement = document.getElementById(imgId);
+    draw(x, y, context) {
+        const imgId = "frame" + this.type;
+        const imgElement = document.getElementById(imgId);
         context.drawImage(imgElement, x, y, imgSize * SCALE, imgSize * SCALE);
-    };
-    return Tile;
-}());
-var setupDrawBoard = function () {
-    var canvas = document.getElementById("drawBoard");
-    var ctx = canvas.getContext("2d");
+    }
+}
+const setupDrawBoard = () => {
+    const canvas = document.getElementById("drawBoard");
+    const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, SIDE_SIZE, SIDE_SIZE);
     return ctx;
 };
-var start = function () {
-    var context = setupDrawBoard();
-    var tile = new Tile(1);
-    var tile2 = new Tile(2);
+const start = () => {
+    const context = setupDrawBoard();
+    const tile = new Tile(1);
+    const tile2 = new Tile(2);
     tile.draw(0, 0, context);
     tile2.draw(48, 0, context);
 };
