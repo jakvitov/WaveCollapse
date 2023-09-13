4a9068388ee40ffdf65aa0abd39c78ba0ec8f894
jakvitov <jakvitov@gmail.com>
2023-09-13 19:34:28 +0200

    First collapse concept implemented

--- build/wave_collapse.js
+++ build/wave_collapse.js
@@ -1,5 +1,4 @@
-const imgSize = 48;
-const SCALE = 1;
+const IMG_SIZE = 48;
 const SIDE_SIZE = 720;
 var Direction;
 (function (Direction) {
@@ -8,6 +7,37 @@ var Direction;
     Direction[Direction["LEFT"] = 2] = "LEFT";
     Direction[Direction["RIGHT"] = 3] = "RIGHT";
 })(Direction || (Direction = {}));
+//Data structure, a map, that helps us to store coordinates in a map (since normal map would)
+//store them as pointer, not a value
+class CoordMap {
+    constructor() {
+        this.map = new Map();
+    }
+    coordToString(coord) {
+        return coord.x.toFixed(0) + "_" + coord.y.toFixed(0);
+    }
+    stringToCoord(strCoord) {
+        const parsedStr = strCoord.split("_");
+        const x = parseInt(parsedStr[0]);
+        const y = parseInt(parsedStr[1]);
+        return { x: x, y: y };
+    }
+    set(key, val) {
+        this.map.set(this.coordToString(key), val);
+    }
+    get(key) {
+        return this.map.get(this.coordToString(key));
+    }
+    has(key) {
+        return this.map.has(this.coordToString(key));
+    }
+    delete(key) {
+        this.map.delete(this.coordToString(key));
+    }
+    clear() {
+        this.map.clear();
+    }
+}
 //Class representing rules for each type of tile
 class Rules {
     constructor() {
@@ -21,17 +51,48 @@ class Rules {
 }
 //Basic class representing a tile on a board with a default picture from /frames directory
 class Tile {
-    constructor(type) {
-        this.type = type;
+    //We create the tile with all possibilites
+    constructor() {
+        this.type = new Set([0, 1, 2, 3]);
     }
     //Draw the tile, the x,y are left-upper corner
-    draw(x, y, context) {
-        const imgId = "frame" + this.type;
+    draw(coord, context) {
+        if (!this.isCollapsed) {
+            throw "Not collapsed";
+        }
+        const imgId = "frame" + this.getType();
         const imgElement = document.getElementById(imgId);
-        context.drawImage(imgElement, x, y, imgSize * SCALE, imgSize * SCALE);
+        context.drawImage(imgElement, coord.x, coord.y, IMG_SIZE, IMG_SIZE);
+    }
+    //Return true if the wave function of this tile is collapsed
+    isCollapsed() {
+        return this.type.size === 1;
+    }
+    //Return entropy as number -> we measure it as number of states
+    //that the tile is in
+    getEntropy() {
+        return this.type.size;
+    }
+    //Restrict the type of this tile accordingly to allowed set of values 
+    restrictType(allowed) {
+        this.type.forEach((type) => {
+            if (!allowed.has(type)) {
+                this.type.delete(type);
+            }
+        });
+    }
+    //Returns the type of the Tile if it has already collapsed
+    getType() {
+        //Now we know, that the size is 1, so we can just return the first item
+        return this.type.values().next().value;
+    }
+    //Set state of this Tile as a random one of the given states
+    setStateAsRandom() {
+        const randomPosition = Math.floor(Math.random() * (this.type.size));
+        this.type = new Set([Array.from(this.type)[randomPosition]]);
     }
     //Creates default rules for tiles from the /frames directory
-    createDefaultRules() {
+    static createDefaultRules() {
         let result = new Rules();
         let rulesForZero = new Map();
         rulesForZero.set(Direction.UP, new Set([2, 3]));
@@ -60,17 +121,99 @@ class Tile {
         return result;
     }
 }
-const setupDrawBoard = () => {
-    const canvas = document.getElementById("drawBoard");
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, SIDE_SIZE, SIDE_SIZE);
-    return ctx;
-};
+//The board is a top level facade for all Rules, Tiles and drawing related logic
+//We want to only interact with the board as for all wave function algorithm from outside 
+class Board {
+    constructor() {
+        this.board = new CoordMap();
+        this.sideTiles = SIDE_SIZE / IMG_SIZE;
+        //We fill the board with empty tiles
+        for (let i = 0; i < this.sideTiles; i++) {
+            for (let k = 0; k < this.sideTiles; k++) {
+                this.board.set({ x: i, y: k }, new Tile());
+            }
+        }
+        this.rules = Tile.createDefaultRules();
+        this.context = this.setupCanvas();
+    }
+    setupCanvas() {
+        const canvas = document.getElementById("drawBoard");
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, SIDE_SIZE, SIDE_SIZE);
+        return ctx;
+    }
+    //Return tile on the board with minimal entropy
+    //We use this tile as the next one to collapse to lower the collision risk
+    getMinimalEntropyTileCoord() {
+        let minCoord = { x: 0, y: 0 };
+        let minEntropy = this.board.get(minCoord).getEntropy();
+        for (let i = 0; i < this.sideTiles; i++) {
+            for (let k = 0; k < this.sideTiles; k++) {
+                const currentCoord = { x: i, y: k };
+                const currentEntropy = this.board.get(currentCoord).getEntropy();
+                if (minEntropy > currentEntropy) {
+                    minEntropy = currentEntropy;
+                    minCoord = currentCoord;
+                }
+            }
+        }
+        return minCoord;
+    }
+    //Collapse a given tile
+    collapseTile(coord) {
+        const collapsingTile = this.board.get(coord);
+        // We apply the rules only if the tile exists (canvas borders) and is collapsed
+        const upTile = this.board.get({ x: coord.x, y: coord.y + 1 });
+        if (upTile !== undefined && upTile.isCollapsed()) {
+            //We get all the possibilities down from the up-tile
+            const possibilites = this.rules.rules.get(upTile.type[0]).get(Direction.DOWN);
+            //We delete all restricted possibilities from the types of the collapsing tile
+            collapsingTile.restrictType(possibilites);
+        }
+        const downTile = this.board.get({ x: coord.x, y: coord.y - 1 });
+        if (downTile !== undefined && downTile.isCollapsed()) {
+            const possibilites = this.rules.rules.get(downTile.type[0]).get(Direction.UP);
+            //We delete all restricted possibilities from the types of the collapsing tile
+            collapsingTile.restrictType(possibilites);
+        }
+        const rightTile = this.board.get({ x: coord.x + 1, y: coord.y });
+        if (rightTile !== undefined && rightTile.isCollapsed()) {
+            const possibilites = this.rules.rules.get(rightTile.type[0]).get(Direction.LEFT);
+            //We delete all restricted possibilities from the types of the collapsing tile
+            collapsingTile.restrictType(possibilites);
+        }
+        const leftTile = this.board.get({ x: coord.x + 1, y: coord.y });
+        if (leftTile !== undefined && leftTile.isCollapsed()) {
+            const possibilites = this.rules.rules.get(leftTile.type[0]).get(Direction.RIGHT);
+            //We delete all restricted possibilities from the types of the collapsing tile
+            collapsingTile.restrictType(possibilites);
+        }
+        //We have no possibilites to chose from -> contradiction
+        //We have to start over from somewhere (for simple wfa implementation)
+        //In more advanced one, we could backtrack
+        if (collapsingTile.getEntropy() < 1) {
+            throw "Contradiction!";
+        }
+        console.log("Collapsing tile [" + coord.x + ", " + coord.y + "] with " + collapsingTile.getEntropy() + " possibilities");
+        //We chose random state from the allowed ones
+        collapsingTile.setStateAsRandom();
+        console.log("Collapsed tile [" + coord.x + ", " + coord.y + "] to type " + collapsingTile.getType());
+    }
+    //Return a coord of a random tile, that belongs to the canvas
+    //We use this to start the collapse at the beginning, later we use lowest entropy tile, not random
+    getRandomTileCoord() {
+        const x = Math.floor(Math.random() * (this.sideTiles));
+        const y = Math.floor(Math.random() * (this.sideTiles));
+        return { x: x, y: y };
+    }
+    renderTile(tile) {
+        this.board.get(tile).draw(tile, this.context);
+    }
+}
 const start = () => {
-    const context = setupDrawBoard();
-    const tile = new Tile(1);
-    const tile2 = new Tile(2);
-    tile.draw(0, 0, context);
-    tile2.draw(48, 0, context);
+    const brd = new Board();
+    const tile = brd.getRandomTileCoord();
+    brd.collapseTile(tile);
+    brd.renderTile(tile);
 };
 document.getElementById("startButton").addEventListener("click", start);
--- wave_collapse.ts
+++ wave_collapse.ts
@@ -1,7 +1,6 @@
 const IMG_SIZE : number = 48;
 const SIDE_SIZE : number = 720;
 
-
 enum Direction {
     UP,
     DOWN,
@@ -12,6 +11,51 @@ enum Direction {
 interface Coord {
     x : number;
     y : number;
+}
+
+
+//Data structure, a map, that helps us to store coordinates in a map (since normal map would)
+//store them as pointer, not a value
+class CoordMap<Z>{
+
+    //We serialize the coord value into a string, because strings in TypeScript maps
+    //are search by value, not a pointer
+    private map : Map<string, Z>;
+
+    constructor(){
+        this.map = new Map<string, Z>();
+    }
+
+    private coordToString(coord : Coord) : string {
+        return coord.x.toFixed(0) + "_" + coord.y.toFixed(0);
+    }
+
+    private stringToCoord(strCoord : string) : Coord {
+        const parsedStr : Array<string> = strCoord.split("_");
+        const x : number = parseInt(parsedStr[0]);
+        const y : number = parseInt(parsedStr[1]);
+        return {x : x, y : y};
+    }
+
+    set(key: Coord, val : Z) : void {
+        this.map.set(this.coordToString(key), val);
+    }
+
+    get(key : Coord) : Z {
+        return this.map.get(this.coordToString(key));
+    }
+
+    has(key : Coord) : boolean {
+        return this.map.has(this.coordToString(key));
+    }
+
+    delete(key : Coord) : void {
+        this.map.delete(this.coordToString(key));
+    }
+
+    clear() : void {
+        this.map.clear();
+    }
 }
 
 //Class representing rules for each type of tile
@@ -40,18 +84,18 @@ class Tile {
     type : Set<number>;
 
     //We create the tile with all possibilites
-    constructor(coord : Coord){
+    constructor(){
         this.type = new Set<number>([0, 1, 2, 3]);
     }
 
     //Draw the tile, the x,y are left-upper corner
-    draw(x : Number, y: Number, context : any) : void {
+    draw(coord : Coord, context : any) : void {
         if (!this.isCollapsed){
             throw "Not collapsed"
         }
-        const imgId : string = "frame" + this.type[0];
+        const imgId : string = "frame" + this.getType();
         const imgElement : HTMLElement = document.getElementById(imgId);
-        context.drawImage(imgElement, x, y, IMG_SIZE, IMG_SIZE);
+        context.drawImage(imgElement, coord.x, coord.y, IMG_SIZE, IMG_SIZE);
     }
 
     //Return true if the wave function of this tile is collapsed
@@ -72,6 +116,18 @@ class Tile {
                 this.type.delete(type);
             }
         })
+    }
+
+    //Returns the type of the Tile if it has already collapsed
+    getType() : number {
+        //Now we know, that the size is 1, so we can just return the first item
+        return this.type.values().next().value;
+    }
+
+    //Set state of this Tile as a random one of the given states
+    setStateAsRandom(){
+        const randomPosition : number = Math.floor(Math.random() * (this.type.size));
+        this.type = new Set<number>([Array.from(this.type)[randomPosition]]);
     }
 
     //Creates default rules for tiles from the /frames directory
@@ -110,32 +166,44 @@ class Tile {
     }
 }
 
-class Board{
+//The board is a top level facade for all Rules, Tiles and drawing related logic
+//We want to only interact with the board as for all wave function algorithm from outside 
+class Board {
 
-    board : Map<Coord, Tile>;
+    context : any;
+    board : CoordMap<Tile>;
     rules : Rules;
+    //Number of images we can get to the board both vertically and horizontally 
+    sideTiles : number;
 
     constructor(){
-        const sideTiles : number = SIDE_SIZE/IMG_SIZE;
+        this.board = new CoordMap<Tile>();
+        this.sideTiles = SIDE_SIZE/IMG_SIZE;
         //We fill the board with empty tiles
-        for (let i : number = 0; i < sideTiles; i ++){
-            for (let k : number = 0; k < sideTiles; k ++){
-                this.board.set({x: i, y : k}, new Tile({x: i, y : k}))
+        for (let i : number = 0; i < this.sideTiles; i ++){
+            for (let k : number = 0; k < this.sideTiles; k ++){
+                this.board.set({x: i, y : k}, new Tile());
             }
         }
         this.rules = Tile.createDefaultRules();
+        this.context = this.setupCanvas();
     }
 
+    private setupCanvas() : any {
+        const canvas : any= document.getElementById("drawBoard");
+        const ctx : any= canvas.getContext("2d");
+        ctx.clearRect(0, 0, SIDE_SIZE, SIDE_SIZE);
+        return ctx;
+    }
+    
     //Return tile on the board with minimal entropy
     //We use this tile as the next one to collapse to lower the collision risk
     getMinimalEntropyTileCoord() : Coord {
         let minCoord : Coord = {x: 0, y: 0}; 
         let minEntropy: number = this.board.get(minCoord).getEntropy();
 
-        const sideTiles : number = SIDE_SIZE/IMG_SIZE;
-
-        for (let i : number = 0; i < sideTiles; i ++){
-            for (let k : number = 0; k < sideTiles; k ++){
+        for (let i : number = 0; i < this.sideTiles; i ++){
+            for (let k : number = 0; k < this.sideTiles; k ++){
                 const currentCoord : Coord = {x : i, y : k}
                 const currentEntropy : number = this.board.get(currentCoord).getEntropy();
                 if (minEntropy > currentEntropy){
@@ -150,53 +218,76 @@ class Board{
 
     //Collapse a given tile
     collapseTile(coord : Coord){
+
+        const collapsingTile : Tile = this.board.get(coord);
+
+        // We apply the rules only if the tile exists (canvas borders) and is collapsed
         const upTile : Tile = this.board.get({x : coord.x, y : coord.y + 1});
-        if (upTile && upTile.isCollapsed){
+        if (upTile !== undefined && upTile.isCollapsed()){
             //We get all the possibilities down from the up-tile
             const possibilites : Set<number> = this.rules.rules.get(upTile.type[0]).get(Direction.DOWN);
             //We delete all restricted possibilities from the types of the collapsing tile
-            this.board.get(coord).restrictType(possibilites);
+            collapsingTile.restrictType(possibilites);
         }
 
         const downTile : Tile = this.board.get({x : coord.x, y : coord.y - 1});
-        if (downTile && downTile.isCollapsed){
-            //We get all the possibilities down from the up-tile
+        if (downTile !== undefined && downTile.isCollapsed()){
             const possibilites : Set<number> = this.rules.rules.get(downTile.type[0]).get(Direction.UP);
             //We delete all restricted possibilities from the types of the collapsing tile
-            this.board.get(coord).restrictType(possibilites);
+            collapsingTile.restrictType(possibilites);
         }
 
         const rightTile : Tile = this.board.get({x : coord.x + 1, y : coord.y});
-        if (rightTile && rightTile.isCollapsed){
-            //We get all the possibilities down from the up-tile
+        if (rightTile !== undefined && rightTile.isCollapsed()){
             const possibilites : Set<number> = this.rules.rules.get(rightTile.type[0]).get(Direction.LEFT);
             //We delete all restricted possibilities from the types of the collapsing tile
-            this.board.get(coord).restrictType(possibilites);
+            collapsingTile.restrictType(possibilites);
         }
 
         const leftTile : Tile = this.board.get({x : coord.x + 1, y : coord.y});
-        if (leftTile && leftTile.isCollapsed){
-            //We get all the possibilities down from the up-tile
-            const possibilites : Set<number> = this.rules.rules.get(leftTile.type[0]).get(Direction.LEFT);
+        if (leftTile !== undefined && leftTile.isCollapsed()){
+            const possibilites : Set<number> = this.rules.rules.get(leftTile.type[0]).get(Direction.RIGHT);
             //We delete all restricted possibilities from the types of the collapsing tile
-            this.board.get(coord).restrictType(possibilites);
+            collapsingTile.restrictType(possibilites);
         }
+
+        //We have no possibilites to chose from -> contradiction
+        //We have to start over from somewhere (for simple wfa implementation)
+        //In more advanced one, we could backtrack
+        if (collapsingTile.getEntropy() < 1){
+            throw "Contradiction!"
+        }
+
+        console.log("Collapsing tile [" + coord.x + ", " + coord.y + "] with " + collapsingTile.getEntropy() + " possibilities")
+
+        //We chose random state from the allowed ones
+        collapsingTile.setStateAsRandom();
+
+        console.log("Collapsed tile [" + coord.x + ", " + coord.y + "] to type " + collapsingTile.getType());
     }
 
-const setupDrawBoard = () : any => {
-    const canvas : any= document.getElementById("drawBoard");
-    const ctx : any= canvas.getContext("2d");
-    ctx.clearRect(0, 0, SIDE_SIZE, SIDE_SIZE);
-    return ctx;
+    //Return a coord of a random tile, that belongs to the canvas
+    //We use this to start the collapse at the beginning, later we use lowest entropy tile, not random
+    getRandomTileCoord() : Coord {
+        const x : number = Math.floor(Math.random() * (this.sideTiles));
+        const y : number = Math.floor(Math.random() * (this.sideTiles));
+        return {x : x, y : y};
+    }
+
+    renderTile(tile : Coord) : void {
+        this.board.get(tile).draw(tile, this.context);
+    }
 }
 
 
 const start = () => {
+    const brd : Board = new Board();
 
-    const context : any = setupDrawBoard();
+    const tile : Coord = brd.getRandomTileCoord(); 
+    brd.collapseTile(tile);
 
-
-
+    brd.renderTile(tile);
+    
 }
 
 document.getElementById("startButton").addEventListener("click", start);
